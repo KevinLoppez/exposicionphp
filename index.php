4f95c001ddbd1d0046a5cb78f694f117b46e746a
Kevin Lopez <ksloez709@misena.edu.co>
2022-11-22 07:47:10 -0500

    v3

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once __DIR__. '/../vendor/autoload.php';
 
 $invoice = new Invoice();
 
-echo serialize();
-echo serialize();
-echo serialize();
+$str = serialize($invoice);
+
+$invoice2 = unserialize($str);
+
+var_dump($invoice,$invoice2, $invoice ===$invoice2);
